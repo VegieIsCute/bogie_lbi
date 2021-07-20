1064c4756085841a0cfb069a4a11fdb05f1f3035
BegieIsCute <49800712+VegieIsCute@users.noreply.github.com>
2021-07-20 19:40:44 -0400

    improved code
    0x90 yelled at me :(

--- bogie lbi v2/Addys.h
+++ bogie lbi v2/Addys.h
@@ -1,101 +1,59 @@
-#define aslr(x) (x - 0x400000 + (DWORD)GetModuleHandle(0))
-#define r_lua_pop(rL,n)		r_lua_settop(rL, -(n)-1)
+const auto ModuleHandle = reinterpret_cast<std::uintptr_t>(GetModuleHandleA(NULL));
+const auto aslr(std::uintptr_t Address) { return (Address - 0x400000u + ModuleHandle); }
 
-struct RBXTValue
-{
-    Value obj;
-    std::uintptr_t pad;
-    int type;
-};
+//Offsets
+constexpr auto instance = 12;
 
-#define ls_top 16
+//Addresses
+const auto lua_getfield_addr = aslr(0x01648E00u);
+const auto lua_setfield_addr = aslr(0x0164A4A0u);
+const auto lua_newthread_addr = aslr(0x01649310u);
+const auto lua_pushlstring_addr = aslr(0x01649890u);
+const auto lua_settop_addr = aslr(0x0164A7C0u);
+const auto lua_pcall_addr = aslr(0x01649580u);
+const auto luau_deserialize_addr = aslr(0x01655490u);
+const auto getdatamodel_addr = aslr(0x01077B30u);
+const auto getdatamodel2_addr = aslr(0x010779E0u);
 
-#define R_LUA_TFUNCTION  9
-
-#define lua_newthread_addr 0x01649310
+//Calling conventions
+#define lua_getfield_CCV __cdecl
+#define lua_setfield_CCV __cdecl
 #define lua_newthread_CCV __cdecl
-
-#define lua_settop_addr 0x0164A7C0
-#define lua_settop_CCV __cdecl
-
-#define luau_deserialize_addr 0x01655490
-#define luau_deserialize_CCV __cdecl
-
-#define lua_pcall_addr 0x01649580
-#define lua_pcall_CCV __cdecl
-
-#define lua_topointer_addr 0x0164AB70
-#define lua_topointer_CCV __cdecl
-
-#define lua_pushlstring_addr 0x01649890
 #define lua_pushlstring_CCV __cdecl
-
-#define getdatamodel_addr 0x01077B30
+#define lua_settop_CCV __cdecl
+#define lua_pcall_CCV __cdecl
+#define luau_deserialize_CCV __cdecl
 #define getdatamodel_CCV __thiscall
-
-#define getdatamodel2_addr 0x010779E0
 #define getdatamodel2_CCV __cdecl
 
-#define lua_setfield_addr 0x0164A4A0
-#define lua_setfield_CCV __cdecl
+const auto r_lua_getfield = reinterpret_cast<void(lua_getfield_CCV*)(std::uintptr_t, int, const char*)>(unprotect(lua_getfield_addr));
+const auto r_lua_setfield = reinterpret_cast<void(lua_setfield_CCV*)(std::uintptr_t, int, const char*)>(unprotect(lua_setfield_addr));
+const auto r_lua_newthread = reinterpret_cast<std::uintptr_t(lua_newthread_CCV*)(std::uintptr_t)>(unprotect(lua_newthread_addr));
+const auto r_lua_pushlstring = reinterpret_cast<void(lua_pushlstring_CCV*)(std::uintptr_t, const char*, size_t)> (unprotect(lua_pushlstring_addr));
+const auto r_lua_settop = reinterpret_cast<void(lua_settop_CCV*)(std::uintptr_t, int)>(unprotect(lua_settop_addr));
+const auto r_lua_pcall = reinterpret_cast<void(lua_pcall_CCV*)(std::uintptr_t, int, int, int)>(unprotect(lua_pcall_addr));
+const auto r_lua_pop(std::uintptr_t state, int n) { r_lua_settop(state, -(n)-1); }
 
-typedef void(getdatamodel_CCV* DEF_getdatamodel1)(std::uintptr_t gdm2, std::uintptr_t pad);
-DEF_getdatamodel1 getdatamodel1 = reinterpret_cast<DEF_getdatamodel1>(unprotect(aslr(getdatamodel_addr)));
+const auto deserialize = reinterpret_cast<void(luau_deserialize_CCV*)(std::uintptr_t, const char*, const char*, size_t, bool)>(unprotect(luau_deserialize_addr));
+const auto getdatamodel1 = reinterpret_cast<void(getdatamodel_CCV*)(std::uintptr_t, std::uintptr_t)>(unprotect(getdatamodel_addr));
+const auto getdatamodel2 = reinterpret_cast<std::uintptr_t(getdatamodel2_CCV*)()>(unprotect(getdatamodel2_addr));
 
-typedef std::uintptr_t(getdatamodel2_CCV* DEF_getdatamodel2)();
-DEF_getdatamodel2 getdatamodel2 = reinterpret_cast<DEF_getdatamodel2>(unprotect(aslr(getdatamodel2_addr)));
+const auto state_get(std::uintptr_t ScriptContext) { return  *reinterpret_cast<std::uintptr_t*>(ScriptContext + 276) + (ScriptContext + 276); }
 
-typedef std::uintptr_t(lua_newthread_CCV* DEF_lua_newthread)(std::uintptr_t state);
-DEF_lua_newthread r_lua_newthread = reinterpret_cast<DEF_lua_newthread>(unprotect(aslr(lua_newthread_addr)));
-
-typedef void(lua_settop_CCV* DEF_lua_settop)(std::uintptr_t state, int idx);
-DEF_lua_settop r_lua_settop = reinterpret_cast<DEF_lua_settop>(unprotect(aslr(lua_settop_addr)));
-
-typedef int(luau_deserialize_CCV* DEF_deserialize)(std::uintptr_t state, const char* chunkname, const char* buffer, size_t bufferlen, bool usestackenv);
-DEF_deserialize deserialize = reinterpret_cast<DEF_deserialize>(unprotect(aslr(luau_deserialize_addr)));
-
-typedef int(lua_pcall_CCV* DEF_lua_pcall)(std::uintptr_t state, int args, int resultidx, int errcallbackidx);
-DEF_lua_pcall r_lua_pcall = reinterpret_cast<DEF_lua_pcall>(unprotect(aslr(lua_pcall_addr)));
-
-typedef std::uintptr_t(lua_topointer_CCV* DEF_lua_topointer)(std::uintptr_t state, int idx);
-DEF_lua_topointer r_lua_topointer = reinterpret_cast<DEF_lua_topointer>(unprotect(aslr(lua_topointer_addr)));
-
-typedef void(lua_pushlstring_CCV* DEF_lua_pushlstring)(std::uintptr_t state, const char* buffer, size_t len);
-DEF_lua_pushlstring r_lua_pushlstring = reinterpret_cast<DEF_lua_pushlstring>(unprotect(aslr(lua_pushlstring_addr)));
-
-typedef void(lua_setfield_CCV* DEF_lua_setfield)(std::uintptr_t state, int idx, const char* name);
-DEF_lua_setfield r_lua_setfield = reinterpret_cast<DEF_lua_setfield>(unprotect(aslr(lua_setfield_addr)));
-
-namespace GDM {
-
-    std::uintptr_t GetDataModel()
-    {
-        std::uintptr_t DMPad[8];
-        getdatamodel1(getdatamodel2(), reinterpret_cast<std::uintptr_t>(DMPad));
-        return DMPad[0] + 0xC;
-    }
-
-    int FindFirstClass(int Instance, const char* Name)
-    {
-        uintptr_t StartOfChildren = *reinterpret_cast<uintptr_t*>(Instance + 44);
-        uintptr_t EndOfChildren = *reinterpret_cast<uintptr_t*>(StartOfChildren + 4);
-        for (int i = *reinterpret_cast<int*>(StartOfChildren); i != EndOfChildren; i += 8)
-        {
-            if (!memcmp(reinterpret_cast<const char*>((*(int(**)(void))(*reinterpret_cast<int*>(*reinterpret_cast<int*>(i)) + 16))()), Name, strlen(Name)))
-            {
-                return *reinterpret_cast<int*>(i);
-            }
-        }
-    }
+const auto getdatamodel() {
+    std::uintptr_t pad[8];
+    getdatamodel1(getdatamodel2(), reinterpret_cast<std::uintptr_t>(pad));
+    return pad[0] + instance;
 }
 
-void r_lua_pushrawobject(std::uintptr_t state, std::uintptr_t obj, int type)
+const auto findfirstchildofclass(std::uintptr_t inst, const char* nclass)
 {
-    RBXTValue*& rawobj = *reinterpret_cast<RBXTValue**>(state + ls_top);
-    rawobj->type = type;
-    rawobj->obj.p = reinterpret_cast<void*>(obj);
-    (rawobj)++;
-}
+    const auto start = *reinterpret_cast<std::uintptr_t*>(inst + 44);
+    const auto end = *reinterpret_cast<std::uintptr_t*>(start + 4);
 
-DWORD RBX_LuaState(DWORD ScriptContext) { return  *(DWORD*)(ScriptContext + 276) + (ScriptContext + 276); }
-DWORD RBX_SetIdentity(DWORD LS, int identity) { *(DWORD*)(*(DWORD*)(LS + 108) + 24) = identity; return 0; }
+    for (int i = *reinterpret_cast<int*>(start); i != end; i += 8)
+        if (!memcmp(reinterpret_cast<const char*>((*(int(**)(void))(*reinterpret_cast<int*>(*reinterpret_cast<int*>(i)) + 16))()), nclass, strlen(nclass)))   
+            return *reinterpret_cast<int*>(i);
+    
+    return 0;
+}
